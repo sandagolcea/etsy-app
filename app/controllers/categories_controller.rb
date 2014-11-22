112e3e5ffe3c46712f9e55e4341507e5312c6f38
Sanda Golcea <sanda.golcea@gmail.com>
2014-11-22 22:56:11 +0000

    CONTROLLER, VIEW: edit, update

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,9 +8,30 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to(:action => 'index')
+    else
+      render('new')
+    end
   end
 
   def edit
+    @category = Category.find(params[:id])
+  end
+
+  
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      redirect_to(:action => 'show', :id => @category.id)
+    else
+      render('index')
+    end
   end
 
   def delete
